a884ede2ec1ed153631bb236b5d69a4425b77a6b
Joseph Adamo <josephaadamo67@gmail.com>
2022-03-18 13:55:59 -0500

    Step 3, drawLibrary renders a card for each book in myLibrary array

--- librarian.js
+++ librarian.js
@@ -19,8 +19,46 @@ function addBookToLibrary(newBook) {
 
 const container = document.querySelector('#container');
 
+addBookToLibrary(book1);
+
+addBookToLibrary(book2);
 
 function drawLibrary() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        const card = document.createElement('div');
+        card.classList.add("card");
+        const bookTitle = document.createElement('h2');
+        bookTitle.textContent = myLibrary[i].title;
+        bookTitle.classList.add("bookTitle")
+        card.appendChild(bookTitle);
+
+        const bookAuthor = document.createElement('h2');
+        bookAuthor.textContent = myLibrary[i].author;
+        bookAuthor.classList.add("bookAuthor")
+        card.appendChild(bookAuthor);
+
+        const bookPages = document.createElement('h2');
+        bookPages.textContent = myLibrary[i].pages;
+        bookPages.classList.add("bookPages")
+        card.appendChild(bookPages);
+
+        const bookRead = document.createElement('h2');
+        bookRead.textContent = myLibrary[i].read;
+        bookRead.classList.add("bookRead")
+        card.appendChild(bookRead);
+
+        container.appendChild(card);
+    }
 
 
 }
+
+
+
+// const container = document.querySelector('#container');
+
+// const content = document.createElement('div');
+// content.classList.add('content');
+// content.textContent = 'This is the glorious text-content!';
+
+// container.appendChild(content);
