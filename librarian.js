1c0bc586ef94be4605522f212b1d6b13e65df642
Joseph Adamo <josephaadamo67@gmail.com>
2022-03-18 16:23:29 -0500

    Step 4, HTML form to add a book to library. Refactored drawLibrary

--- librarian.js
+++ librarian.js
@@ -19,46 +19,80 @@ function addBookToLibrary(newBook) {
 
 const container = document.querySelector('#container');
 
+
+function drawLibrary() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        createBook(myLibrary[i]);
+    }
+}
+
+function createBook(book) {
+    const card = document.createElement('div');
+    card.classList.add("card");
+    const bookTitle = document.createElement('h2');
+    bookTitle.textContent = book.title;
+    bookTitle.classList.add("bookTitle")
+    card.appendChild(bookTitle);
+
+    const bookAuthor = document.createElement('h2');
+    bookAuthor.textContent = book.author;
+    bookAuthor.classList.add("bookAuthor")
+    card.appendChild(bookAuthor);
+
+    const bookPages = document.createElement('h2');
+    bookPages.textContent = book.pages;
+    bookPages.classList.add("bookPages")
+    card.appendChild(bookPages);
+
+    const bookRead = document.createElement('h2');
+    bookRead.textContent = book.read;
+    bookRead.classList.add("bookRead")
+    card.appendChild(bookRead);
+
+    container.appendChild(card);
+}
+//  addbook form
+let modal = document.getElementById("addBookModal");
+let btn = document.getElementById("addBook");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+let form = document.getElementById('addBookForm');
+
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let title = document.getElementById('form-book-title').value;
+    let author = document.getElementById('form-book-author').value;
+    let pages = document.getElementById('form-book-pages').value;
+    let read = document.getElementById('book-read').value;
+    const book = new Book(title, author, pages, read);
+    addBookToLibrary(book);
+    drawLibrary();
+
+});
+
+
+
 addBookToLibrary(book1);
 
 addBookToLibrary(book2);
-
-function drawLibrary() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        const card = document.createElement('div');
-        card.classList.add("card");
-        const bookTitle = document.createElement('h2');
-        bookTitle.textContent = myLibrary[i].title;
-        bookTitle.classList.add("bookTitle")
-        card.appendChild(bookTitle);
-
-        const bookAuthor = document.createElement('h2');
-        bookAuthor.textContent = myLibrary[i].author;
-        bookAuthor.classList.add("bookAuthor")
-        card.appendChild(bookAuthor);
-
-        const bookPages = document.createElement('h2');
-        bookPages.textContent = myLibrary[i].pages;
-        bookPages.classList.add("bookPages")
-        card.appendChild(bookPages);
-
-        const bookRead = document.createElement('h2');
-        bookRead.textContent = myLibrary[i].read;
-        bookRead.classList.add("bookRead")
-        card.appendChild(bookRead);
-
-        container.appendChild(card);
-    }
-
-
-}
-
-
-
-// const container = document.querySelector('#container');
-
-// const content = document.createElement('div');
-// content.classList.add('content');
-// content.textContent = 'This is the glorious text-content!';
-
-// container.appendChild(content);
+drawLibrary();
